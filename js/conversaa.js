33df0efaac07cf17325abff903732edb40d412ae
Fabio Cerqueira <fabiocerqueiracw@gmail.com>
2016-03-04 10:56:54 +0100

    Passing the conversaa url through the conversaaPixel() parameters

--- js/conversaa.js
+++ js/conversaa.js
@@ -1,13 +1,12 @@
-jQuery.conversaaPixel = function conversaaPixel() {
+jQuery.conversaaPixel = function conversaaPixel(urlTracking) {
   var title = escape(document.title);
   var url = window.location.href;
   var ref = document.referrer;
   var userLang = navigator.language || navigator.userLanguage;
-  var trackingPixel = conversaaUrl + "/mtracking.gif?url=" + encodeURIComponent(url) + "&title=" + encodeURIComponent(title)+ "&language=" + encodeURIComponent(userLang)+"&referrer="+ref;
+  var trackingPixel = urlTracking + "/mtracking.gif?url=" + encodeURIComponent(url) + "&title=" + encodeURIComponent(title)+ "&language=" + encodeURIComponent(userLang)+"&referrer="+ref;
   return trackingPixel;
 }
 jQuery(document).ready(function($) {
-  var pixel = $.conversaaPixel();
-  console.log(pixel);
+  var pixel = $.conversaaPixel(conversaaUrl);
   $('body').append("<img src='"+pixel+"' />");
 });
