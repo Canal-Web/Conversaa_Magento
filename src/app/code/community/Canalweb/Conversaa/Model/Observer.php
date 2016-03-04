a24bfe75f4551e7d051756891ef8a44f0c042702
Fwagra <fabiocerqueiracw@gmail.com>
2016-03-04 17:30:11 +0100

    New fields for Oauth2 authentication

--- src/app/code/community/Canalweb/Conversaa/Model/Observer.php
+++ src/app/code/community/Canalweb/Conversaa/Model/Observer.php
@@ -3,18 +3,19 @@ class Canalweb_Conversaa_Model_Observer
 {
     public function addJavascriptBlock($observer)
     {
-        Mage::log('aie');
         $controller = $observer->getAction();
         $layout = $controller->getLayout();
         $conversaa_url = Mage::getStoreConfig('conversaa/main/conversaa_url',Mage::app()->getStore());
-        $scriptjs = '
-        <script type="text/javascript">
-            // Create var with our value from back-office settings (needed in js/conversaa.js)
-            conversaaUrl = "' . $conversaa_url . '"
-        </script>
-        ';
-        $block = $layout->createBlock('core/text');
-        $block->setText($scriptjs);
-        $layout->getBlock('head')->append($block);
+        if(!empty($conversaa_url)){
+            $scriptjs = '
+            <script type="text/javascript">
+                // Create var with our value from back-office settings (needed in js/conversaa.js)
+                conversaaUrl = "' . $conversaa_url . '"
+            </script>
+            ';
+            $block = $layout->createBlock('core/text');
+            $block->setText($scriptjs);
+            $layout->getBlock('head')->append($block);
+        }
     }
 }
