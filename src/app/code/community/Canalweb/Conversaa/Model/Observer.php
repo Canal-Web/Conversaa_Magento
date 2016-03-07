ed5fb1e134daa1ee9749f55b5bd8521b95fe3436
Fwagra <fabiocerqueiracw@gmail.com>
2016-03-07 16:18:50 +0100

    Fix conversaa_method

--- src/app/code/community/Canalweb/Conversaa/Model/Observer.php
+++ src/app/code/community/Canalweb/Conversaa/Model/Observer.php
@@ -6,15 +6,16 @@ class Canalweb_Conversaa_Model_Observer
         $controller = $observer->getAction();
         $layout = $controller->getLayout();
         $conversaa_url = Mage::getStoreConfig('conversaa/main/conversaa_url',Mage::app()->getStore());
+        $conversaa_method =  Mage::getStoreConfig('conversaa/main/conversaa_method',Mage::app()->getStore());
         if(!empty($conversaa_url)){
-            if($conversaa_url == 'js'){
+            if($conversaa_method == 'js'){
                 $content = '
                 <script type="text/javascript">
                     // Create var with our value from back-office settings (needed in js/conversaa.js)
                     conversaaUrl = "' . $conversaa_url . '"
                 </script>
                 ';
-            }elseif ($conversaa_url == 'php') {
+            }elseif ($conversaa_method == 'php') {
                 $headBlock = $this->getLayout()->getBlock('head');
                 $title = ($headBlock)? $headBlock->getTitle() : '';
                 $mail = Mage::getSingleton('customer/session')->getCustomer()->getEmail();
