821cfabbf83bae24e4d605f21e1222d0224b25e7
Fwagra <fabiocerqueiracw@gmail.com>
2016-03-07 11:59:45 +0100

    Choice of tracking mode (php or js)

--- src/app/code/community/Canalweb/Conversaa/Model/Observer.php
+++ src/app/code/community/Canalweb/Conversaa/Model/Observer.php
@@ -7,14 +7,27 @@ class Canalweb_Conversaa_Model_Observer
         $layout = $controller->getLayout();
         $conversaa_url = Mage::getStoreConfig('conversaa/main/conversaa_url',Mage::app()->getStore());
         if(!empty($conversaa_url)){
-            $scriptjs = '
-            <script type="text/javascript">
-                // Create var with our value from back-office settings (needed in js/conversaa.js)
-                conversaaUrl = "' . $conversaa_url . '"
-            </script>
-            ';
+            if($conversaa_url == 'js'){
+                $content = '
+                <script type="text/javascript">
+                    // Create var with our value from back-office settings (needed in js/conversaa.js)
+                    conversaaUrl = "' . $conversaa_url . '"
+                </script>
+                ';
+            }elseif ($conversaa_url == 'php') {
+                $headBlock = $this->getLayout()->getBlock('head');
+                $title = ($headBlock)? $headBlock->getTitle() : '';
+                $mail = Mage::getSingleton('customer/session')->getCustomer()->getEmail();
+                $d = urlencode(base64_encode(serialize(array(
+                    'page_url'   => 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'],
+                    'page_title' => $title,
+                    'email' => $mail
+                ))));
+
+                $content = '<img src="http://'.$conversaa_url.'/mtracking.gif?d=' . $d . '" style="display: none;" />';
+            }
             $block = $layout->createBlock('core/text');
-            $block->setText($scriptjs);
+            $block->setText($content);
             $layout->getBlock('head')->append($block);
         }
     }
